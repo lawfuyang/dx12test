f5e3692b616a9d51419834459ea95bf50950d11a
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2020-03-18 00:09:48 +0800

    Added missing camera controller files from prevous submit

--- src/application/cameracontroller.cpp
+++ src/application/cameracontroller.cpp
@@ -5,61 +5,109 @@
 void CameraController::Initialize()
 {
     Reset();
+
+    m_MouseLastPos = { Mouse::GetX(), Mouse::GetY() };
 }
 
-XMMATRIX CameraController::Get3DViewProjMatrix()
+Matrix CameraController::Get3DViewProjMatrix()
 {
     const float aspectRatio = (float)g_CommandLineOptions.m_WindowWidth / (float)g_CommandLineOptions.m_WindowHeight; // TODO: retrieve window dimensions from somewhere legit
-    const float fovAngleY = XMConvertToRadians(m_FOV);
+    const float fovAngleY = DirectX::XMConvertToRadians(m_FOV);
 
-    const XMVECTOR focusPosition = m_EyePosition + m_Dir;
-    const XMMATRIX viewMatrix = XMMatrixTranspose(XMMatrixLookAtLH(m_EyePosition, focusPosition, m_UpDirection));
-    const XMMATRIX projMatrix = XMMatrixTranspose(XMMatrixPerspectiveFovLH(fovAngleY, aspectRatio, m_Near, m_Far));
+    const Vector3 focusPosition = m_EyePosition + m_Dir;
+    const Matrix viewMatrix = Matrix::CreateLookAtLH(m_EyePosition, focusPosition, m_UpDirection).Transpose();
+    const Matrix projMatrix = Matrix::CreatePerspectiveFieldOfViewLH(fovAngleY, aspectRatio, m_Near, m_Far).Transpose();
 
     return projMatrix * viewMatrix;
 }
 
-void CameraController::Update()
+void CameraController::UpdateEyePosition()
 {
+    Vector3 finalMoveVector = Vector3::Zero;
+
     if (Keyboard::IsKeyPressed(Keyboard::KEY_A))
     {
-        m_EyePosition += { -0.1f, 0.0f, 0.0f, 0.0f };
+        finalMoveVector -= m_RightDirection;
     }
-    else if (Keyboard::IsKeyPressed(Keyboard::KEY_D))
+    if (Keyboard::IsKeyPressed(Keyboard::KEY_D))
     {
-        m_EyePosition += { 0.1f, 0.0f, 0.0f, 0.0f };
+        finalMoveVector += m_RightDirection;
     }
-    else if (Keyboard::IsKeyPressed(Keyboard::KEY_W))
+    if (Keyboard::IsKeyPressed(Keyboard::KEY_W))
     {
-        m_EyePosition += { 0.0f, 0.0f, 0.1f, 0.0f };
+        finalMoveVector += m_Dir;
     }
-    else if (Keyboard::IsKeyPressed(Keyboard::KEY_S))
+    if (Keyboard::IsKeyPressed(Keyboard::KEY_S))
     {
-        m_EyePosition += { 0.0f, 0.0f, -0.1f, 0.0f };
+        finalMoveVector -= m_Dir;
     }
+
+    finalMoveVector.Normalize();
+    m_EyePosition += finalMoveVector * m_CameraMoveSpeed;
+}
+
+void CameraController::UpdateCameraRotation()
+{
+    const Vector2 mousePos{ Mouse::GetX(), Mouse::GetY() };
+    const Vector2 mouseDeltaVec = mousePos - m_MouseLastPos;
+
+    // compute new camera angles and vectors based off mouse delta
+    m_HorizontalAngle -= m_MouseRotationSpeed * float(mouseDeltaVec.x);
+    m_VerticalAngle += m_MouseRotationSpeed * float(mouseDeltaVec.y);
+
+    m_Dir =
+    {
+        std::cos(m_VerticalAngle) * std::sin(m_HorizontalAngle),
+        std::sin(m_VerticalAngle),
+        std::cos(m_VerticalAngle) * std::cos(m_HorizontalAngle),
+    };
+
+    m_RightDirection =
+    {
+        std::sin(m_HorizontalAngle - bbePIBy2),
+        0,
+        std::cos(m_HorizontalAngle - bbePIBy2),
+    };
 
     // lock mouse to center of window when rotating camera
+    // get engine window dimensions
+    ::RECT clientRect = {};
+    ::GetClientRect(g_System.GetEngineWindowHandle(), &clientRect);
+
+    // get engine window position in monitor screen coords
+    ::POINT clientToScreen = {};
+    ::ClientToScreen(g_System.GetEngineWindowHandle(), &clientToScreen);
+
+    // lock mouse to center of window, in monitor screen coords
+    ::SetCursorPos(clientToScreen.x + (clientRect.right / 2), clientToScreen.y + (clientRect.bottom / 2));
+
+    // set last mouse pos to center of window, in window coords
+    m_MouseLastPos = { (float)g_CommandLineOptions.m_WindowWidth / 2, (float)g_CommandLineOptions.m_WindowHeight / 2 };
+}
+
+void CameraController::Update()
+{
+    UpdateEyePosition();
+
     if (Mouse::IsButtonPressed(Mouse::Right))
     {
-        // get engine window dimensions
-        ::RECT clientRect = {};
-        ::GetClientRect(g_System.GetEngineWindowHandle(), &clientRect);
-
-        // get engine window position in monitor screen coords
-        ::POINT clientToScreen = {};
-        ::ClientToScreen(g_System.GetEngineWindowHandle(), &clientToScreen);
-        
-        // lock mouse to center of window
-        ::SetCursorPos(clientToScreen.x + (clientRect.right / 2), clientToScreen.y + (clientRect.bottom / 2));
+        UpdateCameraRotation();
+    }
+    else
+    {
+        m_MouseLastPos = { Mouse::GetX(), Mouse::GetY() };
     }
 
-    const XMMATRIX viewProjMatrix = Get3DViewProjMatrix();
+    m_UpDirection = m_RightDirection.Cross(m_EyePosition);
+
+    const Matrix viewProjMatrix = Get3DViewProjMatrix();
     g_GfxView.SetViewProjMatrix(viewProjMatrix);
 }
 
 void CameraController::Reset()
 {
-    m_EyePosition = { 0.0f, 0.0f, -1.0f, 1.0f };
-    m_Dir = { 0.0f, 0.0f, 1.0f, 0.0f };
-    m_UpDirection = { 0.0f, 1.0f, 0.0f, 0.0f };
+    m_EyePosition = { 0.0f, 0.0f, -1.0f };
+    m_Dir = { 0.0f, 0.0f, 1.0f };
+    m_UpDirection = { 0.0f, 1.0f, 0.0f };
+    m_RightDirection = { 1.0f, 0.0f, 0.0f };
 }
--- src/application/cameracontroller.h
+++ src/application/cameracontroller.h
@@ -10,14 +10,23 @@ public:
 
 private:
     void Reset();
-    XMMATRIX CameraController::Get3DViewProjMatrix();
+    Matrix CameraController::Get3DViewProjMatrix();
+    void UpdateEyePosition();
+    void UpdateCameraRotation();
 
     float m_Near = 0.01f;
     float m_Far  = 100.0f;
     float m_FOV  = 90.0f;
 
-    XMVECTOR m_EyePosition; // Where the camera is in world space. Z increases into of the screen when using LH coord system (which we are and DX uses)
-    XMVECTOR m_Dir;         // Direction that the camera is looking at
-    XMVECTOR m_UpDirection; // Which way is up
+    float m_VerticalAngle = 0.0f;
+    float m_HorizontalAngle = 0.0f;
+    float m_MouseRotationSpeed = 0.002f;
+    float m_CameraMoveSpeed = 0.1f;
+    Vector2 m_MouseLastPos = {};
+
+    Vector3 m_EyePosition; // Where the camera is in world space. Z increases into of the screen when using LH coord system (which we are and DX uses)
+    Vector3 m_Dir;         // Direction that the camera is looking at
+    Vector3 m_UpDirection; // Which way is up
+    Vector3 m_RightDirection;
 };
 #define g_CameraController CameraController::GetInstance()
