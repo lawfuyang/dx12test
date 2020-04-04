88f12f17fa6846aa6ae366b8e043fd4a53b368a2
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2020-04-04 18:56:47 +0800

    Made StaticStringBase inherit directly from std::basic_string
    So we dont have to use operator->, which looks stupid

--- src/system/staticstring.h
+++ src/system/staticstring.h
@@ -1,89 +1,75 @@
 #pragma once
 
 template <typename CharType>
-class StackAllocator
+class StaticStringAllocator
 {
 public:
     using value_type = CharType;
 
-    StackAllocator() noexcept {}
+    StaticStringAllocator() noexcept {}
 
     template <class U> 
-    StackAllocator(const StackAllocator<U>&) noexcept {}
+    StaticStringAllocator(const StaticStringAllocator<U>&) noexcept {}
 
-    explicit StackAllocator(std::byte* bufferSource)
-        : m_BufferSource((value_type*)bufferSource)
+    explicit StaticStringAllocator(value_type* bufferSource) noexcept
+        : m_BufferSource(bufferSource)
     {}
 
-    value_type* allocate(std::size_t n)
-    {
-        assert(m_BufferSource);
-        return m_BufferSource;
-    }
+    value_type* allocate(std::size_t n) noexcept { return m_BufferSource; }
+    constexpr void deallocate(value_type* p, std::size_t n) noexcept  {}
 
-    constexpr void deallocate(value_type* p, std::size_t n) {}
-
-    value_type* m_BufferSource = nullptr;
-};
-
-template <uint32_t BufferSize, typename CharT>
-class StaticStringBase
-{
-public:
-    using UnderlyingStringType = std::basic_string<CharT, std::char_traits<CharT>, StackAllocator<CharT>>;
-
-    UnderlyingStringType* operator->() { return &m_UnderlyingStringType; }
-    const UnderlyingStringType* operator->() const { return &m_UnderlyingStringType; }
-
-    operator UnderlyingStringType() const { return m_UnderlyingStringType; }
-
-    StaticStringBase()
-        : m_Allocator(m_Buffer)
-        , m_UnderlyingStringType(m_Allocator)
-    {
-        m_UnderlyingStringType.reserve(BufferSize);
-    }
-
-    StaticStringBase(const CharT* other)
-        : StaticStringBase()
-    {
-        if (other)
-        {
-            m_UnderlyingStringType = other;
-        }
-    }
-
-    StaticStringBase(const StaticStringBase& other)
-        : StaticStringBase()
-    {
-        m_UnderlyingStringType = other.m_UnderlyingStringType;
-    }
-
-    StaticStringBase& operator=(const StaticStringBase& other)
-    {
-        m_UnderlyingStringType = other.m_UnderlyingStringType;
-        return *this;
-    }
-
-    StaticStringBase& operator+=(const StaticStringBase& other)
-    {
-        m_UnderlyingStringType += other.m_UnderlyingStringType;
-        return *this;
-    }
-
-    bool operator<(const StaticStringBase& other) const { return m_UnderlyingStringType < other.m_UnderlyingStringType; }
-
-    bool operator==(const StaticStringBase& other) const { return m_UnderlyingStringType == other.m_UnderlyingStringType; }
-    bool operator!=(const StaticStringBase& other) const { return m_UnderlyingStringType != other.m_UnderlyingStringType; }
-
-    CharT& operator[](std::size_t idx) { return m_UnderlyingStringType[idx]; }
-    const CharT operator[](std::size_t idx) const { return m_UnderlyingStringType[idx]; }
+    value_type* GetBuffer() const noexcept { return m_BufferSource; }
 
 private:
-    StackAllocator<CharT> m_Allocator;
-    UnderlyingStringType  m_UnderlyingStringType;
-    std::byte             m_Buffer[BufferSize];
+    value_type* m_BufferSource;
 };
+
+template <typename CharType>
+bool operator==(const StaticStringAllocator<CharType>& lhs, const StaticStringAllocator<CharType>& rhs) noexcept
+{
+    return lhs.GetBuffer() == rhs.GetBuffer();
+}
+
+template <typename CharType>
+bool operator!=(const StaticStringAllocator<CharType>& lhs, const StaticStringAllocator<CharType>& rhs) noexcept
+{
+    return !(lhs == rhs);
+}
+
+template <uint32_t BufferSize, typename CharType>
+class StaticStringBase : protected StaticStringAllocator<CharType>,
+                         public std::basic_string<CharType, std::char_traits<CharType>, StaticStringAllocator<CharType>>
+{
+public:
+    using AllocatorType = StaticStringAllocator<CharType>;
+    using Base          = std::basic_string<CharType, std::char_traits<CharType>, AllocatorType>;
+
+    operator Base() { return (Base)*this; }
+
+    template <typename ...Params>
+    StaticStringBase(Params&&... params)
+        : AllocatorType(m_Buffer)
+        , Base(std::forward<Params>(params)..., (AllocatorType)*this)
+    {
+        Base::reserve(BufferSize);
+    }
+
+private:
+    CharType m_Buffer[BufferSize];
+};
+
+template <uint32_t BufferSize, typename CharType>
+bool operator==(const StaticStringBase<BufferSize, CharType>& lhs, const StaticStringBase<BufferSize, CharType>& rhs) noexcept
+{
+    using Base = StaticStringBase<BufferSize, CharType>::Base;
+    return static_cast<const Base&>(lhs) == static_cast<const Base&>(rhs);
+}
+
+template <uint32_t BufferSize, typename CharType>
+bool operator!=(const StaticStringBase<BufferSize, CharType>& lhs, const StaticStringBase<BufferSize, CharType>& rhs) noexcept
+{
+    return !(lhs == rhs);
+}
 
 template <uint32_t BufferSize>
 using StaticString = StaticStringBase<BufferSize, char>;
--- src/unittests/staticstring_test.cpp
+++ src/unittests/staticstring_test.cpp
@@ -5,41 +5,33 @@ static void StaticStringUnitTest_Construction()
 {
     {
         StaticString<5> stringTest;
-        assert(stringTest->empty());
-        assert(stringTest->capacity() >= 5);
-        assert(stringTest->length() == 0);
+        assert(stringTest.empty());
+        assert(stringTest.capacity() >= 5);
+        assert(stringTest.length() == 0);
     }
 
     {
-        StaticString<32> stringTest2("ABCD");
-        assert(stringTest2 == "ABCD");
-        assert(stringTest2->empty() == false);
-        assert(stringTest2->capacity() >= 32);
-        assert(stringTest2->length() == 4);
+        StaticString<64> stringTest2("The quick brown fox jumps over the lazy dog");
+        assert(stringTest2 == "The quick brown fox jumps over the lazy dog");
+        assert(stringTest2.empty() == false);
+        assert(stringTest2.capacity() >= 64);
+        assert(stringTest2.length() == 43);
     }
 
     {
         StaticString<32> stringTest3("");
         assert(stringTest3 == "");
-        assert(stringTest3->empty());
-        assert(stringTest3->capacity() >= 32);
-        assert(stringTest3->length() == 0);
-    }
-
-    {
-        StaticString<32> stringTest4(nullptr);
-        assert(stringTest4 == "");
-        assert(stringTest4->empty());
-        assert(stringTest4->capacity() >= 32);
-        assert(stringTest4->length() == 0);
+        assert(stringTest3.empty());
+        assert(stringTest3.capacity() >= 32);
+        assert(stringTest3.length() == 0);
     }
 
     {
         StaticString<8> stringTest5("Ubi");
         StaticString<8> stringTest6{ stringTest5 };
         assert(stringTest6 == "Ubi");
-        assert(stringTest6->capacity() >= 8);
-        assert(stringTest6->length() == 3);
+        assert(stringTest6.capacity() >= 8);
+        assert(stringTest6.length() == 3);
     }
 }
 
@@ -59,22 +51,22 @@ static void StaticStringUnitTest_Comparison()
 
     // Start With
     {
-        assert(stringTestU->find("U") == 0);
-        assert(stringTestU->find("Ubi") == 0);
-        assert(stringTestU->find("Ubis") != 0);
-        assert(stringTestU->find("u") != 0);
-        assert(stringTestU->find("uBI") != 0);
-        assert(stringTestU->find("UBIS") != 0);
+        assert(stringTestU.find("U") == 0);
+        assert(stringTestU.find("Ubi") == 0);
+        assert(stringTestU.find("Ubis") != 0);
+        assert(stringTestU.find("u") != 0);
+        assert(stringTestU.find("uBI") != 0);
+        assert(stringTestU.find("UBIS") != 0);
     }
 
     // Clear
     {
         StaticString<64> stringTestUCopy{ stringTestU };
-        stringTestUCopy->clear();
+        stringTestUCopy.clear();
 
-        assert(stringTestUCopy->empty());
-        assert(stringTestUCopy->capacity() >= 64);
-        assert(stringTestUCopy->length() == 0);
+        assert(stringTestUCopy.empty());
+        assert(stringTestUCopy.capacity() >= 64);
+        assert(stringTestUCopy.length() == 0);
     }
 
     // Lower Upper case
@@ -82,13 +74,13 @@ static void StaticStringUnitTest_Comparison()
         StaticString<64> stringTestUCopy{ stringTestU };
         StaticString<64> stringTestSCopy{ stringTestS };
 
-        std::transform(stringTestUCopy->begin(), stringTestUCopy->end(), stringTestUCopy->begin(), [](char c) { return std::toupper(c); });
+        std::transform(stringTestUCopy.begin(), stringTestUCopy.end(), stringTestUCopy.begin(), [](char c) { return std::toupper(c); });
         assert(stringTestUCopy == "UBI");
 
-        std::transform(stringTestUCopy->begin(), stringTestUCopy->end(), stringTestUCopy->begin(), [](char c) { return std::tolower(c); });
+        std::transform(stringTestUCopy.begin(), stringTestUCopy.end(), stringTestUCopy.begin(), [](char c) { return std::tolower(c); });
         assert(stringTestUCopy == "ubi");
 
-        std::transform(stringTestSCopy->begin(), stringTestSCopy->end(), stringTestSCopy->begin(), [](char c) { return std::tolower(c); });
+        std::transform(stringTestSCopy.begin(), stringTestSCopy.end(), stringTestSCopy.begin(), [](char c) { return std::tolower(c); });
         assert(stringTestSCopy == "soft");
     }
 
@@ -105,7 +97,6 @@ static void StaticStringUnitTest_Comparison()
         assert(stringTestUCopy != "");
         assert(stringTestUCopy == stringTestUCopy);
         assert(stringTestUCopy != stringTestSCopy);
-        assert(stringTestUCopy != nullptr);
 
         assert(stringTestUCopy[0] == 'U');
         stringTestUCopy[0] = 'V';
@@ -121,15 +112,15 @@ static void StaticStringUnitTest_Comparison()
         stringTest = StringFormat("Soft:%i", 2018).c_str();
 
         assert(stringTest == "Soft:2018");
-        assert(stringTest->length() == 9);
-        assert(stringTest->capacity() >= 64);
+        assert(stringTest.length() == 9);
+        assert(stringTest.capacity() >= 64);
 
         StaticWString<64> stringTest2(L"Ubi");
         stringTest2 = MakeWStrFromStr(StringFormat("Soft:%i", 2018)).c_str();
 
         assert(stringTest2 == L"Soft:2018");
-        assert(stringTest2->length() == 9);
-        assert(stringTest2->capacity() >= 64);
+        assert(stringTest2.length() == 9);
+        assert(stringTest2.capacity() >= 64);
     }
 
     // Format Append
@@ -138,15 +129,15 @@ static void StaticStringUnitTest_Comparison()
         stringTest += StringFormat("Soft:%i", 2018).c_str();
 
         assert(stringTest == "UbiSoft:2018");
-        assert(stringTest->length() == 12);
-        assert(stringTest->capacity() >= 64);
+        assert(stringTest.length() == 12);
+        assert(stringTest.capacity() >= 64);
 
         StaticWString<64> stringTest2(L"Ubi");
         stringTest2 += MakeWStrFromStr(StringFormat("Soft:%i", 2018)).c_str();
 
         assert(stringTest2 == L"UbiSoft:2018");
-        assert(stringTest2->length() == 12);
-        assert(stringTest2->capacity() >= 64);
+        assert(stringTest2.length() == 12);
+        assert(stringTest2.capacity() >= 64);
     }
 }
 
