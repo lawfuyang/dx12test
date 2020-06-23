2518e243db5e67bc97b73de8216a2ed64305a0c8
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2020-06-23 23:07:44 +0800

    use specific gfx matrices, which are basically transposed matrices

--- src/graphic/view.cpp
+++ src/graphic/view.cpp
@@ -13,13 +13,21 @@ void View::Update()
 {
     const float aspectRatio = (float)g_CommandLineOptions.m_WindowWidth / (float)g_CommandLineOptions.m_WindowHeight;
 
-    m_Projection = CreatePerspectiveFieldOfViewLH(m_FOV, aspectRatio, m_ZNearP, m_ZFarP).Transpose();
-
-    m_View = CreateLookAtLH(m_Eye, m_Eye + m_LookAt, m_Up).Transpose();
+    m_Projection = CreatePerspectiveFieldOfViewLH(m_FOV, aspectRatio, m_ZNearP, m_ZFarP);
+    m_View = CreateLookAtLH(m_Eye, m_Eye + m_LookAt, m_Up);
+    m_VP = m_Projection * m_View;
 
     m_InvView = m_View.Invert();
     m_InvProjection = m_Projection.Invert();
     m_InvVP = m_VP.Invert();
-    m_VP = m_Projection * m_View;
+
+    m_GfxProjection = m_Projection.Transpose();
+    m_GfxView = m_View.Transpose();
+    m_GfxVP = m_GfxProjection * m_GfxView;
+
+    m_GfxInvView = m_GfxProjection.Transpose();
+    m_GfxInvProjection = m_GfxView.Transpose();
+    m_GfxInvVP = m_GfxVP.Transpose();
+
     m_Frustum.Create(m_VP);
 }
--- src/graphic/view.h
+++ src/graphic/view.h
@@ -17,13 +17,19 @@ public:
     bbeVector3 m_Up     = bbeVector3::UnitY;
     bbeVector3 m_Right  = bbeVector3::UnitX;
 
-    bbeMatrix m_RotationMatrix = bbeMatrix::Identity;
     bbeMatrix m_View           = bbeMatrix::Identity;
     bbeMatrix m_Projection     = bbeMatrix::Identity;
     bbeMatrix m_VP             = bbeMatrix::Identity;
     bbeMatrix m_InvView        = bbeMatrix::Identity;
     bbeMatrix m_InvProjection  = bbeMatrix::Identity;
     bbeMatrix m_InvVP          = bbeMatrix::Identity;
+
+    bbeMatrix m_GfxView          = bbeMatrix::Identity;
+    bbeMatrix m_GfxProjection    = bbeMatrix::Identity;
+    bbeMatrix m_GfxVP            = bbeMatrix::Identity;
+    bbeMatrix m_GfxInvView       = bbeMatrix::Identity;
+    bbeMatrix m_GfxInvProjection = bbeMatrix::Identity;
+    bbeMatrix m_GfxInvVP         = bbeMatrix::Identity;
 
     Frustum m_Frustum;
 };
