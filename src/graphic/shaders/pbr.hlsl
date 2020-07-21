ad29d47aefa256646f9e3038176fac4ac247da5c
lawfuyang <53224523+lawfuyang@users.noreply.github.com>
2020-07-22 00:14:31 +0800

    Add a general purpose "EvaluatePBR" function in pbr.hlsl

--- src/graphic/shaders/pbr.hlsl
+++ src/graphic/shaders/pbr.hlsl
@@ -10,7 +10,7 @@ float3 Fresnel_Shlick(in float3 f0, in float3 f90, in float x)
 
 // Burley B. "Physically Based Shading at Disney"
 // SIGGRAPH 2012 Course: Practical Physically Based Shading in Film and Game Production, 2012.
-float Diffuse_Burley(in float NdotL, in float NdotV, in float LdotH, in float roughness)
+float DiffuseBurley(in float NdotL, in float NdotV, in float LdotH, in float roughness)
 {
     float fd90 = 0.5f + 2.f * roughness * LdotH * LdotH;
     return Fresnel_Shlick(1, fd90, NdotL).x * Fresnel_Shlick(1, fd90, NdotV).x;
@@ -44,7 +44,7 @@ float G_Shlick_Smith_Hable(float alpha, float LdotH)
 //      V - eye normal
 //      L - light normal
 //      H - half vector between L & V.
-float3 Specular_BRDF(in float alpha, in float3 specularColor, in float NdotV, in float NdotL, in float LdotH, in float NdotH)
+float3 SpecularBRDF(in float alpha, in float3 specularColor, in float NdotV, in float NdotL, in float LdotH, in float NdotH)
 {
     // Specular D (microfacet normal distribution) component
     float specular_D = Specular_D_GGX(alpha, NdotH);
@@ -64,13 +64,71 @@ float3 Diffuse_IBL(in TextureCube<float3> irradianceTexture, in float3 N)
     return irradianceTexture.Sample(g_AnisotropicWrapSampler, N);
 }
 
-// Approximate specular image based lighting by sampling radiance map at lower mips 
-// according to roughness, then modulating by Fresnel term. 
-float3 Specular_IBL(TextureCube<float3> radianceTexture, in float3 N, in float3 V, in float lodBias, in uint numRadianceMipLevels)
+// Approximate specular image based lighting by sampling radiance map at lower mips according to roughness, then modulating by Fresnel term. 
+float3 Specular_IBL(in TextureCube<float3> radianceTexture, in float3 N, in float3 V, in float lodBias, in uint numRadianceMipLevels)
 {
     float mip = lodBias * numRadianceMipLevels;
     float3 dir = reflect(-V, N);
     return radianceTexture.SampleLevel(g_AnisotropicWrapSampler, dir, mip);
+}
+
+struct EvaluatePBRArgs
+{
+    float3 V;
+    float3 N;
+    float3 lightColor;
+    float3 L;
+    float3 albedo;
+    float roughness;
+    float metallic;
+    float ambientOcclusion;
+    TextureCube<float3> irradianceTexture;
+    TextureCube<float3> radianceTexture;
+    uint numRadianceMipLevels;
+};
+
+// Apply Disney-style PBR
+float3 EvaluatePBR(in EvaluatePBRArgs input)
+{
+    // Specular coefficiant - fixed reflectance value for non-metals
+    static const float kSpecularCoefficient = 0.04;
+
+    const float NdotV = saturate(dot(input.N, input.V));
+
+    // Burley roughness bias
+    const float alpha = input.roughness * input.roughness;
+
+    // Blend base colors
+    const float3 c_diff = lerp(input.albedo, float3(0, 0, 0), input.metallic) * input.ambientOcclusion;
+    const float3 c_spec = lerp(kSpecularCoefficient, input.albedo, input.metallic) * input.ambientOcclusion;
+
+    // Output color
+    float3 finalColor = 0;
+
+    // Half vector
+    const float3 H = normalize(input.L + input.V);
+
+    // products
+    const float NdotL = saturate(dot(input.N, input.L));
+    const float LdotH = saturate(dot(input.L, H));
+    const float NdotH = saturate(dot(input.N, H));
+
+    // Diffuse & specular factors
+    float diffuse_factor = DiffuseBurley(NdotL, NdotV, LdotH, input.roughness);
+    float3 specular = SpecularBRDF(alpha, c_spec, NdotV, NdotL, LdotH, NdotH);
+
+    // Directional light
+    finalColor += NdotL * input.lightColor * (((c_diff * diffuse_factor) + specular));
+
+    // Add diffuse irradiance
+    float3 diffuse_env = Diffuse_IBL(input.irradianceTexture, input.N);
+    finalColor += c_diff * diffuse_env;
+
+    // Add specular radiance 
+    float3 specular_env = Specular_IBL(input.radianceTexture, input.N, input.V, input.roughness, input.numRadianceMipLevels);
+    finalColor += c_spec * specular_env;
+
+    return finalColor;
 }
 
 #endif // #define __PBR_HLSL__
